1cbc63cb25fef3da3118436e069114642cc729d1
Francis Onyach <franblog7@gmail.com>
2023-01-21 04:50:53 -0800

    task 0 & 2

--- sort.h
+++ sort.h
@@ -2,6 +2,9 @@
 #define _SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
+
+/* Macros for bitonis sort(Comparison direction) */
 
 typedef struct listint_s
 {
@@ -9,6 +12,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
